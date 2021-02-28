0f08652de9564343a74e124c30e63e1cd4f2d57f
mfaibrahem <mfaibrahem2013@gmail.com>
2021-02-28 22:29:31 +0200

    update me

--- pages/users/index.jsx
+++ pages/users/index.jsx
@@ -1,5 +1,21 @@
-const UsersList = () => {
+const UsersList = ({ users }) => {
+  console.log(users);
   return 'all users';
 };
 
 export default UsersList;
+
+export const getStaticProps = async () => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+      props: {
+        users: data
+      }
+    };
+  } catch (e) {
+    console.log(e);
+  }
+};
